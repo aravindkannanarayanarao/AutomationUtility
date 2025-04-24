5bec0fc41193cf761bf9e2ac1a97b4eddc31667f
aravindkannanarayanarao <92799581+aravindkannanarayanarao@users.noreply.github.com>
2025-04-24 08:12:47 -0700

    updated

--- UITestMobile/MAUIAndroidUIAutomationUtility/AndroidTools/AndroidTools.cs
+++ UITestMobile/MAUIAndroidUIAutomationUtility/AndroidTools/AndroidTools.cs
@@ -1,12 +1,12 @@
 ﻿using MAUIAndroidUIAutomationUtility.Helper;
-using NuGet;
 using System.Diagnostics;
 
 namespace MAUIAndroidUIAutomationUtility.AndroidTools
 {
     public static class AndroidTool
     {
-        static string AndroidHome ="/Users/aravindkann/Library/Android/sdk";
+        public static string ANDROID_HOME = "/Users/aravindkann/Library/Android/sdk";
+        public static string ANDROID_PLATFORMTOOL = "/Users/aravindkann/Library/Android/sdk/platform-tools";
         public static void InstallApp(string deviceSerial, string appPath)
         {
             try
@@ -27,7 +27,7 @@ namespace MAUIAndroidUIAutomationUtility.AndroidTools
                 }
 
                 // Execute the adb install command
-                CommondExcecute.ExecuteCommand($"adb -s {deviceSerial} install \"{appPath}\"");
+                CommondExcecute.ExecuteCommand($"{ANDROID_PLATFORMTOOL}/adb -s {deviceSerial} install \"{appPath}\"");
             }
             catch (Exception ex)
             {
@@ -50,7 +50,7 @@ namespace MAUIAndroidUIAutomationUtility.AndroidTools
 
                 var packages = new List<string>();
 
-                string result = $"adb -s {deviceSerial} shell pm list packages";
+                string result = $"{ANDROID_PLATFORMTOOL}/adb -s {deviceSerial} shell pm list packages";
                 CommondExcecute.ExecuteCommand(result);
 
                 string[] lines = result.Split(['\r', '\n'], StringSplitOptions.RemoveEmptyEntries);
@@ -98,7 +98,7 @@ namespace MAUIAndroidUIAutomationUtility.AndroidTools
                 }
 
                 var devices = new List<AdbDevice>();
-                string result = "adb devices -l";
+                string result = "{ANDROID_PLATFORMTOOL}/adb devices -l";
                 CommondExcecute.ExecuteCommand(result);
 
                 string[] lines = result.Split(['\r', '\n'], StringSplitOptions.RemoveEmptyEntries);
@@ -153,7 +153,7 @@ namespace MAUIAndroidUIAutomationUtility.AndroidTools
                 {
                     throw new ArgumentNullException(nameof(avdName), "Error: Device name is missing or invalid.");
                 }
-                string emulatorCommand = $"{AndroidHome}/emulator/emulator -avd {avdName}";
+                string emulatorCommand = $"{ANDROID_HOME}/emulator/emulator -avd {avdName}";
                 Process process = new Process()
 
                 {
@@ -197,7 +197,7 @@ namespace MAUIAndroidUIAutomationUtility.AndroidTools
 
             // Kill all running emulator instances
 
-            CommondExcecute.ExecuteCommand($"adb -s {avdName} emu kill");
+            CommondExcecute.ExecuteCommand($"{ANDROID_PLATFORMTOOL}/adb -s {avdName} emu kill");
 
             Console.WriteLine(avdName+" emulator instances have been shut down.");
             }
@@ -223,9 +223,9 @@ namespace MAUIAndroidUIAutomationUtility.AndroidTools
 
             // Kill all running emulator instances
 
-            CommondExcecute.ExecuteCommand($"adb -s {avdName} emu kill");
-            CommondExcecute.ExecuteCommand($"adb emu kill");
-            CommondExcecute.ExecuteCommand("adb kill-server");
+            CommondExcecute.ExecuteCommand($"{ANDROID_PLATFORMTOOL}/adb -s {avdName} emu kill");
+            CommondExcecute.ExecuteCommand($"{ANDROID_PLATFORMTOOL}/adb emu kill");
+            CommondExcecute.ExecuteCommand("{ANDROID_PLATFORMTOOL}/adb kill-server");
             
 
             Console.WriteLine("All emulator instances have been shut down.");
@@ -259,7 +259,7 @@ namespace MAUIAndroidUIAutomationUtility.AndroidTools
                     {
                         FileName = "/bin/bash",
 
-                        Arguments = "-c \"adb shell getprop sys.boot_completed\"",
+                        Arguments = $"-c \"{ANDROID_PLATFORMTOOL}/adb shell getprop sys.boot_completed\"",
 
                         RedirectStandardOutput = true,
 
@@ -272,7 +272,7 @@ namespace MAUIAndroidUIAutomationUtility.AndroidTools
                 process.Start();
                 string output = process.StandardOutput.ReadToEnd().Trim();
                 process.WaitForExit();
-                if (output == "1" || output.IsEmpty()) break;
+                if (output == "1") break;
                 Thread.Sleep(5000); // Wait for 5 seconds before checking again
             }
 
--- UITestMobile/MAUIAndroidUIAutomationUtility/Program.cs
+++ UITestMobile/MAUIAndroidUIAutomationUtility/Program.cs
@@ -16,9 +16,9 @@ class Program
 
         //Android platform
 
-        new Dictionary<string, string> { { "ProjectName", "maui-listview-tests" }, { "SampleName", "BindingAndEvents" }, { "ApplicationID", "com.companyname.bindingandevents" }, { "Platform", "UITests.Android" }, { "EmulatorCommand", "Pixel535" } },
+         new Dictionary<string, string> { { "ProjectName", "maui-listview-tests" }, { "SampleName", "BindingAndEvents" }, { "ApplicationID", "com.companyname.bindingandevents" }, { "Platform", "UITests.Android" }, { "EmulatorCommand", "Pixel_5_API_33" } },
         // new Dictionary<string, string> { { "ProjectName", "maui-listview-tests" }, { "SampleName", "CRbugs" }, { "ApplicationID", "com.companyname.crbugs" }, { "Platform", "UITests.Android" }, { "EmulatorCommand", "Pixel_5" } },
-        // new Dictionary<string, string> { { "ProjectName", "maui-listview-tests" }, { "SampleName", "EmptyView" }, { "ApplicationID", "com.companyname.emptyview" }, { "Platform", "UITests.Android" }, { "EmulatorCommand", "Pixel_5" } },
+        // new Dictionary<string, string> { { "ProjectName", "m aui-listview-tests" }, { "SampleName", "EmptyView" }, { "ApplicationID", "com.companyname.emptyview" }, { "Platform", "UITests.Android" }, { "EmulatorCommand", "Pixel_5" } },
         // new Dictionary<string, string> { { "ProjectName", "maui-listview-tests" }, { "SampleName", "HeaderAndFooter" }, { "ApplicationID", "com.companyname.headerandfooter" }, { "Platform", "UITests.Android" }, { "EmulatorCommand", "Pixel_5" } },
         // new Dictionary<string, string> { { "ProjectName", "maui-listview-tests" }, { "SampleName", "MAUISfListViewHeaderAndFooter" }, { "ApplicationID", "com.companyname.MAUISfListViewHeaderAndFooter" }, { "Platform", "UITests.Android" }, { "EmulatorCommand", "Pixel_5" } },
         // new Dictionary<string, string> { { "ProjectName", "maui-listview-tests" }, { "SampleName", "MAUISfListViewHeaderFooter" }, { "ApplicationID", "com.companyname.MAUISfListViewHeaderFooter" }, { "Platform", "UITests.Android" }, { "EmulatorCommand", "Pixel_5" } },
@@ -28,10 +28,11 @@ class Program
         // new Dictionary<string, string> { { "ProjectName", "maui-listview-tests" }, { "SampleName", "mauisflistviewsortgroupfilter" }, { "ApplicationID", "com.companyname.maui.sflistview.recorditems" }, { "Platform", "UITests.Android" }, { "EmulatorCommand", "Pixel_5" } },
         // new Dictionary<string, string> { { "ProjectName", "maui-listview-tests" }, { "SampleName", "MAUISfListViewRTL" }, { "ApplicationID", "com.companyname.mauisflistviewrtl" }, { "Platform", "UITests.Android" }, { "EmulatorCommand", "Pixel_5" } },
         // new Dictionary<string, string> { { "ProjectName", "maui-listview-tests" }, { "SampleName", "MAUISfListViewSelection" }, { "ApplicationID", "com.companyname.mauisflistviewselection" }, { "Platform", "UITests.Android" }, { "EmulatorCommand", "Pixel_5" } },
-        // new Dictionary<string, string> { { "ProjectName", "maui-listview-tests" }, { "SampleName", "MAUISfListViewSortGroupFilter" }, { "ApplicationID", "com.companyname.mauisflistviewsortgroupfilter" }, { "Platform", "UITests.Android" }, { "EmulatorCommand", "Pixel_5" } },           
+        // new Dictionary<string, string> { { "ProjectName", "maui-listview-tests" }, { "SampleName", "MAUISfListViewSortGroupFilter" }, { "ApplicationID", "com.companyname.mauisflistviewsortgroupfilter" }, { "Platform", "UITests.Android" }, { "EmulatorCommand", "Pixel_5" } },      
+
         //iOS platform
 
-        // new Dictionary<string, string> { { "ProjectName", "maui-listview-tests" }, { "SampleName", "BindingAndEvents" }, { "ApplicationID", "com.companyname.bindingandevents" }, { "Platform", "UITests.iOS" }, { "EmulatorCommand", "2EE8CBD0-4E77-4574-810A-C19A1D9E050D" } },
+         //new Dictionary<string, string> { { "ProjectName", "maui-listview-tests" }, { "SampleName", "BindingAndEvents" }, { "ApplicationID", "com.companyname.bindingandevents" }, { "Platform", "UITests.iOS" }, { "EmulatorCommand", "2EE8CBD0-4E77-4574-810A-C19A1D9E050D" } },
         // new Dictionary<string, string> { { "ProjectName", "maui-listview-tests" }, { "SampleName", "CRbugs" }, { "ApplicationID", "com.companyname.crbugs" }, { "Platform", "UITests.iOS" }, { "EmulatorCommand", "2EE8CBD0-4E77-4574-810A-C19A1D9E050D" } },
         // new Dictionary<string, string> { { "ProjectName", "maui-listview-tests" }, { "SampleName", "EmptyView" }, { "ApplicationID", "com.companyname.emptyview" }, { "Platform", "UITests.iOS" }, { "EmulatorCommand", "2EE8CBD0-4E77-4574-810A-C19A1D9E050D" } },
         // new Dictionary<string, string> { { "ProjectName", "maui-listview-tests" }, { "SampleName", "HeaderAndFooter" }, { "ApplicationID", "com.companyname.headerandfooter" }, { "Platform", "UITests.iOS" }, { "EmulatorCommand", "2EE8CBD0-4E77-4574-810A-C19A1D9E050D" } },
@@ -62,44 +63,40 @@ class Program
     {
         if (project["Platform"] == "UITests.Android")
         {
-
-            string emulator = $"{project["EmulatorCommand"]}";
-            string appPath = $"{documentFolder}/Appium/{project["ProjectName"]}/UITest/Appium/{project["SampleName"]}/{project["SampleName"]}";
-            string testPath = $"{documentFolder}/Appium/{project["ProjectName"]}/UITest/Appium/{project["SampleName"]}/{project["Platform"]}";
-            string publishCommand = "dotnet publish -f net9.0-android -c Release -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=key.keystore -p:AndroidSigningKeyAlias=MauiAlias -p:AndroidSigningKeyPass=kanna007 -p:AndroidSigningStorePass=kanna007";
-            string installCommand = $"/Users/aravindkann/Library/Android/sdk/platform-tools/adb install {appPath}/bin/Release/net9.0-android/publish/{project["ApplicationID"]}-Signed.apk";
-            string TestRun = $"dotnet test {testPath}";
-            Console.WriteLine($"Running commands for {project["ProjectName"]}...");
-            if(project["EmulatorCommand"] == "Pixel535")
+            if(project["EmulatorCommand"] == "Pixel_2_XL")
             {
-            Console.WriteLine($"Starting emulator : {project["EmulatorCommand"]}...");
-            AndroidTool.BootDevice(project["EmulatorCommand"]);
-            }
-            else if(project["EmulatorCommand"] == "Pixel_2")
-            {
-                AndroidTool.ShutdownDeviceCompletely("Pixel_535");
                 Console.WriteLine($"Starting emulator : {project["EmulatorCommand"]}...");
                 AndroidTool.BootDevice(project["EmulatorCommand"]);
             }
+            string appPath = $"{documentFolder}/Appium/{project["ProjectName"]}/UITest/Appium/{project["SampleName"]}/{project["SampleName"]}";
+            string testPath = $"{documentFolder}/Appium/{project["ProjectName"]}/UITest/Appium/{project["SampleName"]}/{project["Platform"]}/{project["Platform"]}.csproj";
+            string publishCommand = "dotnet publish -f net9.0-android -c Release -p:AndroidKeyStore=true -p:AndroidSigningKeyStore=key.keystore -p:AndroidSigningKeyAlias=MauiAlias -p:AndroidSigningKeyPass=kanna007 -p:AndroidSigningStorePass=kanna007";
+            string installCommand = $"{AndroidTool.ANDROID_PLATFORMTOOL}/adb install {appPath}/bin/Release/net9.0-android/publish/{project["ApplicationID"]}-Signed.apk";
+            string TestRun = $"dotnet test {testPath}";
+            Console.WriteLine($"Running commands for {project["ProjectName"]}...");
+            try{
+
             Console.WriteLine($"Starting {project["SampleName"]} build and publish");
             CommondExcecute.ExecuteCommand($"cd {appPath} && {publishCommand}");
 
             Console.WriteLine($"Installing {project["SampleName"]} in to emulator");
             CommondExcecute.ExecuteCommand(installCommand);
 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception sample not installed", ex.Message);
+            }
+            try
+            {
             Console.WriteLine($"UITest started for project : {project["SampleName"]} Sample : {project["SampleName"]} Platform : {project["Platform"]} ");
             CommondExcecute.ExecuteCommand(TestRun);
-            if(project["EmulatorCommand"] == "Pixel535")
-            {
-            //AndroidTool.ShutdownDeviceCompletely("Pixel_535");
-            Console.WriteLine($"Closing emulator");
-            AndroidTool.ShutdownDevice(project["EmulatorCommand"]);
             }
-            else if(project["EmulatorCommand"] == "Pixel_2")
-            {
-            Console.WriteLine($"Closing emulator");
-            AndroidTool.ShutdownDevice(project["EmulatorCommand"]);
-            }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Test Not runned ", ex);
+        }
+            
 
         }
 
